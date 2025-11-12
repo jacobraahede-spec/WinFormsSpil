feed8296664052e52465db820a85b20ce42d10ff
jacobraahede-spec <jacobraahede@gmail.com>
2025-11-12 15:17:37 +0100

    Nye knapper og generel arbejde

--- WinFormsSpil/Form1.cs
+++ WinFormsSpil/Form1.cs
@@ -5,6 +5,19 @@ namespace WinFormsSpil
         public Form1()
         {
             InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form3 f3 = new Form3();
+            f3.Show();
         }
     }
 }
